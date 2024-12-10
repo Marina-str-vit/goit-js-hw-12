37fa5219b73e0e9339209f81c3557bcacbe7299f
Marina-str-vit <meribsv@gmail.comy>
2024-12-10 22:20:40 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,25 +1,77 @@
 import iconReject from '../img/Group_rej.png'
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
 
-const API_KEY = "47392107-146a4706033000661fe0e92fb";
+// const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '47392107-146a4706033000661fe0e92fb';
+// export const searchQuery = "";
+// document.querySelector('.search-input').value.trim();
 
-export function fetchImages(searchQuery) {
-  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(error => {
-      iziToast.error({
-        message: 'Error while receiving images!',
-        position: 'topRight',
-        iconUrl: iconReject,
-        backgroundColor: '#EF4040',
-        messageColor: 'white',
-        timeout: 3000,
-      });
-      console.error(error);
+export async function fetchImages(searchQuery, page = 1) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15&page=${page}`
+    );
+    return response.data;
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Error while receiving images!',
+      position: 'topRight',
+      iconUrl: iconReject,
+      backgroundColor: '#EF4040',
+      messageColor: 'white',
+      timeout: 3000,
     });
+    // console.error(error.message);
+
+    document.querySelector('input').value = '';
+    const loader = document.querySelector('.loader');
+    if (loader) {
+      loader.remove();
+    }
+  }
 }
+
+
+
+
+
+// export async function fetchImages( page = 1) {
+//       const { data } = await axios.get(`${BASE_URL}`, {
+//           params: {
+//               key: API_KEY,
+//               q: searchQuery,
+//               image_type: 'photo',
+//               orientation: 'horizontal',
+//               safesearch: true,
+//               per_page: 15,
+//               page 
+//           }
+//       });
+//       return data;
+      // console.log(data);
+  }
+console.log(fetchImages());
+
+
+// export function fetchImages(searchQuery) {
+//   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+//   return fetch(url)
+//     .then(response => response.json())
+//     .then(data => data)
+//     .catch(error => {
+//       iziToast.error({
+//         message: 'Error while receiving images!',
+//         position: 'topRight',
+//         iconUrl: iconReject,
+//         backgroundColor: '#EF4040',
+//         messageColor: 'white',
+//         timeout: 3000,
+//       });
+//       console.error(error);
+//     });
+// }
 
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { fetchImages } from './js/pixabay-api';
-import { renderSearchForm, renderImageGallery } from './js/render-functions';
+import { renderSearchForm, renderImageGallery, renderLoadMoreButton, renderLoader } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import iconReject from '../src/img/Group_rej.png';
+import iconReject from './img/Group_rej.png'
 
 const main = document.querySelector('main');
 
@@ -15,22 +15,30 @@ const gallery = document.createElement('div');
 gallery.classList.add('gallery');
 main.appendChild(gallery);
 
-const loader = document.createElement('span');
-loader.classList.add('loader');
+const loadMoreButton = renderLoadMoreButton();
+main.appendChild(loadMoreButton);
+loadMoreButton.style.display = 'none';
+
+const loader = renderLoader();
+main.appendChild(loader);
+loader.style.display = 'none';
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captions: true,
 });
 
-searchForm.addEventListener('submit', event => {
+let currentPage = 1;
+let searchQueryGlobal = '';
+const imagesPerPage = 15;
+
+searchForm.addEventListener('submit', async event => {
   event.preventDefault();
-  main.appendChild(loader);
+  loader.style.display = 'block';
   const searchQuery = document.querySelector('#search-input').value.trim();
   if (searchQuery === '') {
-    main.removeChild(loader);
+    loader.style.display = 'none';
     iziToast.error({
       title: 'Error',
-      titleColor: 'white',
       message: 'Please enter your search query!',
       position: 'topRight',
       iconUrl: iconReject,
@@ -41,42 +49,85 @@ searchForm.addEventListener('submit', event => {
     return;
   }
 
+  currentPage = 1;
   gallery.innerHTML = '';
+  loadMoreButton.style.display = 'none';
+  loadMoreButton.disabled = true;
+  searchQueryGlobal = searchQuery;
+  const images = await fetchImages(searchQuery, currentPage);
+  const imageGallery = renderImageGallery(images.hits);
+  imageGallery.forEach(imageCard => {
+    gallery.appendChild(imageCard);
+    lightbox.refresh();
+  });
 
-  fetchImages(searchQuery)
-    .then(images => {
-      main.removeChild(loader);
-
-      if (images.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          iconUrl: iconReject,
-          backgroundColor: '#EF4040',
-          messageColor: 'white',
-          timeout: 3000,
-        });
-      } else {
-        const imageGallery = renderImageGallery(images.hits);
-        imageGallery.forEach(imageCard => {
-          gallery.appendChild(imageCard);
-        });
-        lightbox.refresh();
-      }
-    })
-    .catch(error => {
-      main.removeChild(loader);
-
-      console.error(error);
-      iziToast.error({
-        message: 'Error while receiving images!',
-        position: 'topRight',
-        iconUrl: iconReject,
-        backgroundColor: '#EF4040',
-        messageColor: 'white',
-        timeout: 3000,
-      });
+  if (images.hits.length === 0) {
+    iziToast.error({
+      title: 'No results',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+      iconUrl: iconReject,
+      backgroundColor: '#EF4040',
+      messageColor: 'white',
+      timeout: toastTimeout,
     });
+    loadMoreButton.style.display = 'none';
+    loader.style.display = 'none';
+  } else {
+    if (images.totalHits <= currentPage * imagesPerPage) {
+      loadMoreButton.style.display = 'none';
+      loader.style.display = 'none';
+      document.querySelector('#search-input').value = '';
+      const messageContainer = document.createElement('div');
+      messageContainer.classList.add('end-of-results-message');
+      const message = document.createElement('p');
+      message.textContent =
+        "We're sorry, but you've reached the end of search results.";
+      messageContainer.appendChild(message);
+      gallery.appendChild(messageContainer);
+      lightbox.refresh();
+      return;
+    }
+    loadMoreButton.disabled = images.hits.length < imagesPerPage;
+    if (images.hits.length > 0) {
+      loadMoreButton.style.display = 'block';
+      loader.style.display = 'none';
+    }
+  }
   document.querySelector('#search-input').value = '';
+});
+
+loadMoreButton.addEventListener('click', async () => {
+  loader.style.display = 'block';
+  currentPage +=1;
+  const images = await fetchImages(searchQueryGlobal, currentPage);
+  const imageGallery = renderImageGallery(images.hits);
+  imageGallery.forEach(imageCard => {
+    gallery.appendChild(imageCard);
+  });
+  const imageCard = gallery.children[0];
+  const imageCardHeight = imageCard.getBoundingClientRect().height;
+  window.scrollBy({
+    top: imageCardHeight * 2,
+    behavior: 'smooth',
+  });
+  lightbox.refresh();
+  loader.style.display = 'none';
+  loadMoreButton.disabled = images.hits.length < imagesPerPage;
+
+  if (images.hits.length === 0) {
+    loadMoreButton.style.display = 'none';
+  }
+  if (images.totalHits <= currentPage * imagesPerPage) {
+    loadMoreButton.style.display = 'none';
+    loader.style.display = 'none';
+    const messageContainer = document.createElement('div');
+    messageContainer.classList = 'end-of-results-message';
+    const message = document.createElement('p');
+    message.textContent =
+      "We're sorry, but you've reached the end of search results.";
+    messageContainer.appendChild(message);
+    gallery.appendChild(messageContainer);
+  }
 });
